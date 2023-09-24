99b65903e0058404892ee0cc195b298eb0f03780
mqlv <408654847@qq.com>
2023-09-24 23:26:51 +0800

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -71,14 +71,9 @@ install_ngrok(){
 	export GOPATH=/usr/local/ngrok/
 	export NGROK_DOMAIN=$DOMAIN
 	cd ngrok
-	openssl genrsa -out rootCA.key 2048
-	openssl req -x509 -new -nodes -key rootCA.key -subj "/CN=$NGROK_DOMAIN" -days 5000 -out rootCA.pem
-	openssl genrsa -out server.key 2048
-	openssl req -new -key server.key -subj "/CN=$NGROK_DOMAIN" -out server.csr
-	openssl x509 -req -in server.csr -CA rootCA.pem -CAkey rootCA.key -CAcreateserial -out server.crt -days 5000
-	\cp -rf rootCA.pem assets/client/tls/ngrokroot.crt
-	\cp -rf server.crt assets/server/tls/snakeoil.crt
-	\cp -rf server.key assets/server/tls/snakeoil.key
+	\cp -rf /etc/letsencrypt/live/bravewall.top/chain.pem assets/client/tls/ngrokroot.crt
+	\cp -rf /etc/letsencrypt/live/bravewall.top/cert.pem assets/server/tls/snakeoil.crt
+	\cp -rf /etc/letsencrypt/live/bravewall.top/privkey.pem assets/server/tls/snakeoil.key
 	# 替换下载源地址
 	sed -i 's#code.google.com/p/log4go#github.com/keepeye/log4go#' /usr/local/ngrok/src/ngrok/log/logger.go
 	cd /usr/local/go/src
